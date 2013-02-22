792f947d0f264e04b3e8f4d78b80cf920c2ad581
Chris Bankston <chris@whitepaperclip.com>
2013-02-21 20:41:03 -0600

    Updated add to track user points

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130215222325) do
+ActiveRecord::Schema.define(:version => 20130222023210) do
 
   create_table "spree_activators", :force => true do |t|
     t.string   "description"
@@ -617,6 +617,7 @@ ActiveRecord::Schema.define(:version => 20130215222325) do
     t.integer  "ship_address_id"
     t.integer  "bill_address_id"
     t.string   "username"
+    t.integer  "points"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
