d7db46f594485cd97b5ae8d5e540138ace12cd0d
Chris Bankston <chris@whitepaperclip.com>
2013-03-21 20:09:02 -0500

    Imported the new design into the store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,11 @@ Pointgamingstore::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
