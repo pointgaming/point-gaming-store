9e359254b896a03d89c09223439dd2327ea56e17
Chris Bankston <chris@whitepaperclip.com>
2013-05-03 15:40:52 -0500

    Improved elasticsearch partial matching config

--- app/decorators/models/spree/product_decorator.rb
+++ app/decorators/models/spree/product_decorator.rb
@@ -23,15 +23,14 @@ Spree::Product.class_eval do
       analyzer: {
         partial_match: {
           tokenizer: :whitespace,
-          filter: [:lowercase, :edge_ngram]
+          filter: [:lowercase, :ngram]
         }
       },
       filter: {
-        edge_ngram: {
-            side: :front,
+        ngram: {
             max_gram: 20,
             min_gram: 1,
-            type: :edgeNGram
+            type: :nGram
         }
       }
     } do
