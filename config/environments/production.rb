185ab2e7f6eb17423cbbf03e6c9c223e641970c3
Chris Bankston <chris@whitepaperclip.com>
2013-07-04 22:13:49 -0500

    Moved js includes to the bottom of the body tag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ PointGamingStore::Application.configure do
   config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( pg_start.js )
   config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   config.action_mailer.default_url_options = { :host => 'store.pointgaming.com' }
